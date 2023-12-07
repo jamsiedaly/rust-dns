1337a0b40a242219871856f82002ffa9bc533515
James Daly <jamsiedaly@gmail.com>
2023-12-07 21:20:23 +0100

    First pass implementation of header

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,70 @@
 use std::net::UdpSocket;
 
+pub struct DNSHeader {
+    id: u16,
+    qr: u8,
+    opcode:u8,
+    aa: u8,
+    tc: u8,
+    rd: u8,
+    ra: u8,
+    z: u8,
+    rcode: u8,
+    qdcount: u16,
+    ancount: u16,
+    nscount: u16,
+    arcount: u16,
+}
+
+impl DNSHeader {
+    pub fn serialize(&self) -> [u8; 12] {
+        let mut buffer = [0; 12];
+        buffer[0] = (self.id >> 8) as u8;
+        buffer[1] = self.id as u8;
+        buffer[2] = (self.qr << 7) | (self.opcode << 3) | (self.aa << 2) | (self.tc << 1) | self.rd;
+        buffer[3] = (self.ra << 7) | (self.z << 4) | self.rcode;
+        buffer[4] = (self.qdcount >> 8) as u8;
+        buffer[5] = self.qdcount as u8;
+        buffer[6] = (self.ancount >> 8) as u8;
+        buffer[7] = self.ancount as u8;
+        buffer[8] = (self.nscount >> 8) as u8;
+        buffer[9] = self.nscount as u8;
+        buffer[10] = (self.arcount >> 8) as u8;
+        buffer[11] = self.arcount as u8;
+        return buffer;
+    }
+}
+
+
 fn main() {
     println!("Logs from your program will appear here!");
 
     let udp_socket = UdpSocket::bind("127.0.0.1:2053").expect("Failed to bind to address");
     let mut buf = [0; 512];
 
+    let response_header = DNSHeader {
+        id: 1234,
+        qr: 1,
+        opcode: 0,
+        aa: 0,
+        tc: 0,
+        rd: 0,
+        ra: 0,
+        z: 0,
+        rcode: 0,
+        qdcount: 0,
+        ancount: 0,
+        nscount: 0,
+        arcount: 0,
+    };
+
     loop {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 let _received_data = String::from_utf8_lossy(&buf[0..size]);
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
                 udp_socket
-                    .send_to(&response, source)
+                    .send_to(&response_header.serialize(), source)
                     .expect("Failed to send response");
             }
             Err(e) => {
